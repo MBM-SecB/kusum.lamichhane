863d5f537301a48e3833fa48dea14e56a73c99ae
kusum <kusumlamichhane2015@gmail.com>
2020-10-14 12:24:23 +0545

    Added dropdown to add employee

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,19 +5,26 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        var employees=Employee.GetEmployees();
-        
+        var employees = Employee.GetEmployees();        
         return View(employees);
     }
-    public ActionResult Details(string firstName)
+
+    public ActionResult Detail(string firstName)
     {
         var employees = Employee.GetEmployees();
-        // var employee = employees.Where(x => x.FirstName == firstName).First();
-        // var employee = employees.First(x => x.FirstName == firstName);
         var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
-
-
-        return View(employees);
+        
         return View(employee);
+    }
+    [HttpGet]
+    public ActionResult Add()
+    {
+        return View();
+    }
+    [HttpPost]
+    
+    public ActionResult<bool> Add(Employee employee)
+    {
+        return true;
     }
 }
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,22 +1,29 @@
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+
 public class Employee
 {
+    [Required]
+    [Display(Name ="First Name")]
     public string FirstName { get; set; }
+    [Required]
+    [Display(Name ="Last Name")]
     public string Surname { get; set; }
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
-    public static List <Employee> GetEmployees()
+
+    public static List<Employee> GetEmployees()
     {
         // Object initializer
-        var employee1 = new Employee { FirstName = "kusum", Surname = "lamichhane", Address = "Ktm", Salary = 100.0 };
-        var employee2 = new Employee { FirstName = "unika", Surname = "chaulagain", Address = "Bhaktapur", Salary = 10000.0 };
-        var employee3 = new Employee { FirstName = "hareram", Surname = "lamichhane", Address = "Ktm", Salary = 100.0 };
-        var employee4 = new Employee { FirstName = "laxmi", Surname = "dhungana", Address = "Ktm", Salary = 100.0 };
-        var employee5 = new Employee { FirstName = "hardik", Surname = "kami", Address = "Ktm", Salary = 100.0 };
-        var employee6 = new Employee { FirstName = "bishnuu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
+        var employee1 = new Employee { FirstName = "Top", Surname = "Shrestha", Address = "Ktm", Salary = 10500.0 };
+        var employee2 = new Employee { FirstName = "Shyam", Surname = "Basnet", Address = "Bhaktapur", Salary = 105000.0 };
+        var employee3 = new Employee { FirstName = "Bijay", Surname = "Nepal", Address = "Dang", Salary = 9000.0 };
+        var employee4 = new Employee { FirstName = "Pritam", Surname = "Khatri", Address = "Chitwan", Salary = 11100.0 };
+        var employee5 = new Employee { FirstName = "NiKhil", Surname = "Uppreti", Address = "Lalitpur", Salary = 8500.0 };
+        var employee6 = new Employee { FirstName = "Binay", Surname = "Bajracharya", Address = "Ktm", Salary = 26000.0 };
 
         var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
-        return employees;
+        return employees;     
     }
 }
