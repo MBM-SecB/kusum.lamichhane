5df82d4a3d9ae31d051af6d8be161cf49d64bbee
kusumlamichhane01 <kusumlamichhane2015@gmail.com>
2021-03-10 10:44:46 +0545

    client side state management

--- StateManagement/Controllers/StateController.cs
+++ StateManagement/Controllers/StateController.cs
@@ -1,5 +1,24 @@
-public class StateController:Controller
+using Microsoft.AspNetCore.Mvc;
+
+public class StateController : Controller
 {
+    public ViewResult First()
+    {
+        var name = "Kusum Lamichhane";
+        // CookieOptions
+        Response.Cookies.Append("user", name);
+//
 
-
+        return View();
+    }
+    [HttpGet]
+    public ViewResult Second(string name, int age)
+    {
+        return View();
+    }
+    [HttpPost]
+    public bool Second(string collegeName)
+    {
+        return true;
+    }
 }
